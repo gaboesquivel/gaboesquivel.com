a9061640308e99e6d4bad09a582c8a24bb8706af
gaboesquivel <contact@gaboesquivel.com>
2017-08-08 20:14:33 -0600

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-cp -r public/* _gh-pages
+mkdir _gh-pages
+cp -r public/* /tmp/_gh-pages
 
 cd _gh-pages
+echo $(pwd)
 
 echo 'gaboesquivel.com' > CNAME
 
+
 git init .
 git remote add origin git@github.com:gaboesquivel/gaboesquivel.github.io.git
-
+git fetch origin gh-pages
 git add -A .
 
 git commit -a -m 'gh-pages update'
@@ -16,6 +19,6 @@ git commit -a -m 'gh-pages update'
 git push origin gh-pages --force
 
 cd ..
-rm -rf _gh-pages
+#rm -rf _gh-pages
 
 git checkout master
