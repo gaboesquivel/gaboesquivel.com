3aaf87ed9ad0b7df4156cc41f1db68aa95ee8118
Gabo Esquivel <contact@gaboesquivel.com>
2023-09-10 23:07:58 -0600

    wip: initial content

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
       <p className="prose prose-neutral dark:prose-invert">
         
 
-        I'm a full-stack software engineer with a robust 15+ years of experience, the last 6 years being dedicated to web3 technologies. My background includes work on numerous blockchain projects, a decade with NodeJS and cloud platforms, as well as advanced expertise in containers, React, and TypeScript.
+        I'm a full-stack software engineer with over 15 years of experience, the last 6 years being dedicated to web3 technologies. My background includes work on numerous blockchain projects, a decade with NodeJS and cloud platforms, as well as advanced expertise in containers, React, and TypeScript.
 <p/>
 <p className="prose prose-neutral dark:prose-invert"></p>
 I help companies develop software products and make technical decisions, working with stakeholders and developers in the design and implementation of software applications, taking ideas from conception to production, and successful execution.
