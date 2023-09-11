652015c79f0fb851f60a9643994a78baa9b5b220
Gabo Esquivel <contact@gaboesquivel.com>
2023-09-11 15:47:20 -0600

    chore: og image

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -21,7 +21,7 @@ export async function GET(req: NextRequest) {
           flexDirection: 'column',
           alignItems: 'flex-start',
           justifyContent: 'center',
-          backgroundImage: 'url(https://gaboesquivel.com/og-bg.png)',
+          backgroundImage: 'url(https://gaboesquivel.com/images/gabo-workshop.jpg)',
         }}
       >
         <div
