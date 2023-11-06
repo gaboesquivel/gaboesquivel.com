4d09850c23242c2d575279ce62eb34f42a9458a2
Gabo Esquivel <contact@gaboesquivel.com>
2023-11-06 13:53:52 -0600

    content: update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
       <h1 className="mb-8 text-2xl font-bold tracking-tighter">hi, I'm Gabo Esquivel ( gab-o )</h1>
       
       <p className="prose prose-neutral dark:prose-invert"> 
-        I'm a full-stack software engineer with over 15 years of experience, specializing in web3 technologies since 2017. My background includes work on numerous blockchain projects, a decade with NodeJS and cloud platforms, as well as advanced expertise in containers, React, and TypeScript.
+        I'm a full-stack software engineer with over 15 years of experience, specializing in web3 technologies since 2017. My background includes work on numerous blockchain projects, a decade with NodeJS and cloud platforms, as well as advanced expertise in React, React Native, Docker and TypeScript.
       </p>
 
  <div className="gap-4 my-8 columns-1">
@@ -24,12 +24,16 @@ export default function HomePage() {
  </div>
 
       <p className="prose prose-neutral dark:prose-invert">
-      I advocate for the use of open source technology stacks, decentralized finance (DeFi), a DevOps culture, and streamlined agile methodologies. My primary focus lies at the crossroads of emerging technologies, financial services, and delivering exceptional user experiences.
+        I am an advocate for leveraging open source technologies, embracing the transformative potential of decentralized finance (DeFi), endorsing a DevOps mindset, and applying agile methodologies with precision. My expertise is centered on the fusion of emerging tech, financial service innovations, and the delivery of outstanding user experiences.
       </p>
 
       <p className="prose prose-neutral dark:prose-invert">
-      I help companies develop software products and make technical decisions, working with stakeholders and developers in the design and implementation of software applications, taking ideas from conception to production, and successful execution. I provide my contract services via <a target="_blank" href="https://blockmatic.io">Blockmatic Labs LLC</a>.
-      </p>
+        The allure of cryptocurrency for me lies in its revolutionary offering of asset control and financial independence. Blockchain technology acts as the bedrock for this, providing a strong guarantee of direct asset ownership that sidesteps traditional financial institution constraints. In the realm of DeFi, we’re witnessing profound advancements that pave the way for more inclusive, transparent, and robust financial services. This movement is not just about technological advancements; it’s reshaping how we engage in financial transactions across borders instantly, heralding a new era in economic interaction. The ongoing evolution in this space is a testament to the nascent stages of a more extensive financial transformation.
+       </p>
+
+       <p className="prose prose-neutral dark:prose-invert">
+        Professionally, I assist organizations in crafting and evolving software products, making strategic technical decisions, and collaborating closely with stakeholders and development teams. From initial concept to successful market launch, I am involved in every phase, ensuring ideas are realized to their full potential. My consultancy services are provided through my company, Blockmatic Labs LLC.
+ </p>
 
       <p className="prose prose-neutral dark:prose-invert">
       Blockchain, DeFi, NodeJS, Databases, Linux, Docker, Cloud, GraphQL, React, React Native
