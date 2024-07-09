666e97468fcb388a2c583a1985d8970d1ab6304e
Gabo Esquivel <contact@gaboesquivel.com>
2024-07-09 08:28:00 -0600

    content: missing posts

--- next.config.js
+++ next.config.js
@@ -28,7 +28,7 @@ const ContentSecurityPolicy = `
     default-src 'self' vercel.live;
     script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live;
     style-src 'self' 'unsafe-inline';
-    frame-src 'self' https://www.youtube.com data:;
+    frame-src 'self' https://www.youtube.com https://player.vimeo.com data:;
     img-src * blob: data:;
     media-src 'none';
     connect-src *;
