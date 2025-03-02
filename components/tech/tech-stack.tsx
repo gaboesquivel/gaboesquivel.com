40a7d7a43191a2a1b4e4a65d6661a56a3cde15bc
Gabo Esquivel <contact@gaboesquivel.com>
2025-03-01 21:48:27 -0600

    fix: tech styles

--- components/tech/tech-stack.tsx
+++ components/tech/tech-stack.tsx
@@ -7,13 +7,13 @@ export default function TechStack({
 	return (
 		<div>
 			<h1 className="mb-8 text-2xl font-bold tracking-tighter">{tech.name}</h1>
-			<p>{tech.description}</p>
+			<p className="prose prose-neutral dark:prose-invert">{tech.description}</p>
 			{tech.intro.map((intro) => (
-				<p key={intro}>{intro}</p>
+				<p className="prose prose-neutral dark:prose-invert" key={intro}>{intro}</p>
 			))}
 			<h3 className="font-bold text-xl mb-4">Experience:</h3>
 			{tech.experience.map((experience) => (
-				<p key={experience}>{experience}</p>
+				<p className="prose prose-neutral dark:prose-invert" key={experience}>{experience}</p>
 			))}
 
 			{tech.projects.map((project) => (
