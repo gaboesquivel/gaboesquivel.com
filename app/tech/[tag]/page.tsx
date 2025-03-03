d560de500d1ebf9bdbbf67ec225e3ff76740a578
Gabo Esquivel <contact@gaboesquivel.com>
2025-03-02 20:57:00 -0600

    content: update basilic evm img

--- app/tech/[tag]/page.tsx
+++ app/tech/[tag]/page.tsx
@@ -1,8 +1,8 @@
 import TechStack from 'components/tech/tech-stack';
-import {getTechStackByTag} from 'gaboesquivel';
+import {getTechStackByTag, type Tag} from 'gaboesquivel';
 
 export default function TechPage({params}: {params: {tag: string}}){
-  const tech = getTechStackByTag(params.tag); 
+  const tech = getTechStackByTag(params.tag as Tag); 
   if (!tech) return <div>Tech not found</div>
   return <TechStack tech={tech} />
 }
